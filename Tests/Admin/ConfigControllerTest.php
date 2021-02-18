1f00e2bbc5bceca3641f8ddf138e6736d78623cb
のぶ <nobuhiko.kimoto@gmail.com>
2021-02-18 11:02:30 +0900

    テストのモックを作成

---
+++ Tests/Admin/ConfigControllerTest.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Plugin\DataMigration4\Tests\Web\Admin;
+
+use Eccube\Common\Constant;
+use Eccube\Tests\Web\Admin\AbstractAdminWebTestCase;
+use Symfony\Component\Filesystem\Filesystem;
+
+class ConfigControllerTest extends AbstractAdminWebTestCase
+{
+    public function setUp()
+    {
+        parent::setUp();
+    }
+
+    public function tearDown()
+    {
+        parent::tearDown();
+    }
+
+    public function testのTest()
+    {
+        $this->assertTrue(true, 'これは動いているはずです。');
+    }
+}
